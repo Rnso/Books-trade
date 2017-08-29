10f7029643d9faa901ca2361c9bc0155500adcbd
Rnso <rnso.pnr@gmail.com>
2017-08-29 14:35:49 +0400

    initial upload

--- config.js
+++ config.js
@@ -4,12 +4,11 @@ export const nodeEnv = env.NODE_ENV || 'development'
 
 
 export default{
-    //mongodbUri: 'mongodb://rnso:pongen@ds159892.mlab.com:59892/fcc',
-    mongodbUri: 'mongodb://localhost:27017/bookClub',
+    mongodbUri: 'mongodb://rnso:pongen@ds163053.mlab.com:63053/fcc-books-trade',
+    
     port: env.PORT || 9000,
     host: env.HOST || '0.0.0.0'
 }
-
 
 
 
